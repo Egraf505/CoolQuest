941e70cb7088cce3899fb6aaae164166256e8901
EfgraF505 <egor.leskov.03@mail.ru>
2022-11-05 11:02:19 +0300

    Added class Startup

--- CoolQuest.backend/Program.cs
+++ CoolQuest.backend/Program.cs
@@ -1,33 +1,12 @@
+using CoolQuest.backend;
 using CoolQuest.DbContext.Context;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-// Строка подключение
-string connection = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<CoolQuestContex>(options => options.UseSqlServer(connection));
+CreateHostBuilder(args).Build().Run();
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+ static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+ .ConfigureWebHostDefaults(webBuilder =>
+ {
+     webBuilder.UseStartup<Startup>();
+ });
---
+++ CoolQuest.backend/Startup.cs
@@ -0,0 +1,53 @@
+﻿using CoolQuest.DbContext.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoolQuest.backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Строка подключение
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<CoolQuestContex>(options => options.UseSqlServer(connection));
+
+            // Аутенфикация
+
+
+            // Add services to the container.
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
