191d2c7034be5235e509637d6f6b64a7a1200087
EfgraF505 <egor.leskov.03@mail.ru>
2022-11-16 15:01:14 +0300

    update cors

--- CoolQuest.WebAPI/Program.cs
+++ CoolQuest.WebAPI/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddDbContext<CoolQuestContex>(options => options.UseSqlServer(c
 builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
     builder =>
     {
-        builder.WithOrigins("https://localhost:3000");
+        builder.WithOrigins("https://localhost:3000").AllowAnyMethod();
     }));
 
 // Контроллеры
